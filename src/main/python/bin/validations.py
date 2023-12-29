5ca4465544fbb875cff678552ffee177624afc54
Rajat Nirhale <43401274+rjnirhale@users.noreply.github.com>
2023-12-29 12:50:39 +0530

    Delete src/main/python directory

--- src/main/python/bin/validations.py
+++
@@ -1,7 +0,0 @@
-def get_curr_date(spark):
-    try:
-        opDF = spark.sql(""" SELECT current_date  """)
-        print("Validating spark object by printing current date - " + str(opDF.collect()))
-    except Exception as exp:
-        print("Error in method :", str(exp))
-        raise
